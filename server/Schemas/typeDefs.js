c153f89a8cad34395b41d746ba999d8c27b31f10
Thomas Walker <thomasnwalker@yahoo.com>
2021-09-30 22:19:16 -0700

    typedefs and resolvers

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -1,22 +1,47 @@
 const {gql} = require('apollo-server-express');
 
 const typeDefs = gql`
-type Book {
-    bookId: String
-    authors: [String]
-    description: String
-    title: String
-    image: String
-    link: String
-  }
-  type User {
-    _id: ID
-    username: String
-    email: String
-    bookCount: Int
-    savedBooks: [Book]
-  }
 
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+  
+    input BookInput {
+        bookId: ID
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        deleteBook(bookId: ID!): User
+    }
 
 
 `;
