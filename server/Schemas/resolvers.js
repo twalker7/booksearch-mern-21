2718061b2720a18027bed4a97c110112f2d454a6
Thomas Walker <thomasnwalker@yahoo.com>
2021-09-30 22:13:03 -0700

    starting typedef-resolver pairing

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,6 +1,21 @@
+
+const { User, Book } = require('../models');
+const { signToken } = require('../utils/auth');
+const { AuthenticationError } = require('apollo-server-express');
+
+
 const resolvers = {
     Query:{
-
+        me: async (parent, args, context) => {
+            if(context.user) {
+              const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+            
+            return userData;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+          }
     }
 }
 module.exports = resolvers;
--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -1,6 +1,24 @@
 const {gql} = require('apollo-server-express');
 
 const typeDefs = gql`
+type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+
+
 `;
 
 
